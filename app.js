1e6f2af47f5d3a7193fb09edeb97c96b68d4dca1
DAndreDerr <dandre020727@gmail.com>
2023-06-26 19:55:32 -0400

    got right

--- app.js
+++ app.js
@@ -15,14 +15,6 @@ const connection = mysql.createConnection({
   database:"HopeHacksNews",
 })
 
-const transporter = nodemailer.createTransport({
-  service: 'yahoo',
-  auth: {
-    user: 'hope4changeemail@yahoo.com',
-    pass: 'HopeHacks6!',
-  },
-});
-
 app.use(express.static("public"));
 // using body parser to get user input
 app.use(bodyParser.json());
@@ -32,57 +24,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.set("views", "./src/views");
 
-app.post('/subscribe', (req, res) => {
-  const { email, password } = req.body;
 
-  // Store subscriber data in the database
-  connection.query(
-    'INSERT INTO newsletterData (email, password) VALUES (?, ?)',
-    [email, password],
-    (error, results) => {
-      if (error) {
-        console.error(error);
-        res.send('An error occurred while subscribing.');
-      } else {
-        // Send confirmation email
-        const mailOptions = {
-          from: 'hope4changeemail@yahoo.com',
-          to: email,
-          subject: 'Newsletter Subscription Confirmation',
-          text: 'You have successfully subscribed to our newsletter.',
-        };
-
-        transporter.sendMail(mailOptions, (error, info) => {
-          if (error) {
-            console.error(error);
-          } else {
-            console.log('Email sent: ' + info.response);
-          }
-        });
-
-        res.send('Thank you for subscribing!');
-      }
-    }
-  );
-});
-
-app.post('/unsubscribe', (req, res) => {
-  const { email } = req.body;
-
-  // Remove subscriber data from the database
-  connection.query(
-    'DELETE FROM newsletterData WHERE email = ?',
-    [email],
-    (error, results) => {
-      if (error) {
-        console.error(error);
-        res.send('An error occurred while unsubscribing.');
-      } else {
-        res.send('You have been unsubscribed successfully.');
-      }
-    }
-  );
-});
 
 
 app.get("/support", (req,res) => {
@@ -95,43 +37,43 @@ app.get("/newsletter", (req,res) => {
 	res.render("newsletter")
 })
 
-// app.get('/search', (req, res) => {
-//   const options = {
-//     method: 'GET',
-//     hostname: 'google-news-api1.p.rapidapi.com',
-//     port: null,
-//     path: '/search?language=EN&q=immigration',
-//     headers: {
-//       'X-RapidAPI-Key': 'f0f9c103b4mshf65c80b6eab160fp1e155ajsna30f5f819cb6',
-//       'X-RapidAPI-Host': 'google-news-api1.p.rapidapi.com'
-//     }
-//   };
+app.post('/subscribe', (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
 
-//   const apiRequest = https.request(options, (apiResponse) => {
-//     const chunks = [];
+  // Store subscriber data in the database
+  connection.query(
+    'INSERT INTO subscribers (firstName, lastName, email, password) VALUES (?, ?, ?, ?)',
+    [firstName, lastName, email, password],
+    (error, results) => {
+      if (error) {
+        console.error(error);
+        res.render('newsletter2', { message: 'An error occurred while subscribing.' });
+      } else {
+        res.render('newsletter2', { message: 'Thank you for subscribing!' });
+      }
+    }
+  );
+});
 
-//     apiResponse.on('data', (chunk) => {
-//       chunks.push(chunk);
-//     });
+app.post('/unsubscribe', (req, res) => {
+  const { email, password } = req.body;
 
-//     apiResponse.on('end', () => {
-//       const body = Buffer.concat(chunks);
-//       res.send(body.toString());
-		
-//     });
-
-// 	console.log(chunks);
-//   });
-
-//   apiRequest.on('error', (error) => {
-//     console.error(error);
-//     res.status(500).send('An error occurred');
-//   });
-
- 
-
-//   apiRequest.end();
-// });
+  // Remove subscriber data from the database
+  connection.query(
+    'DELETE FROM subscribers WHERE email = ? AND password = ?',
+    [email, password],
+    (error, results) => {
+      if (error) {
+        console.error(error);
+        res.render('newsletter2', { message: 'An error occurred while unsubscribing.' });
+      } else if (results.affectedRows === 0) {
+        res.render('newsletter2', { message: 'Invalid email or password.' });
+      } else {
+        res.render('newsletter2', { message: 'You have been unsubscribed successfully.' });
+      }
+    }
+  );
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
